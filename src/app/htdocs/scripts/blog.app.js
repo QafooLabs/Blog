d252b3d6fd5da83bd20fefac2d98fc0567edf26a
Kore Nordmann <mail@kore-nordmann.de>
2012-09-26 16:46:40 +0200

    Moved matching logic into main app config

--- src/app/htdocs/scripts/blog.app.js
+++ src/app/htdocs/scripts/blog.app.js
@@ -129,3 +129,38 @@
     global.Blog.App = App;
 
 })(this);
+
+// Globally available variables
+var History;
+
+jQuery().ready(function() {
+    "use strict";
+
+    // Initilialize application
+    var app    = new Blog.App(),
+        router = new Lounge.Router( [
+            {   name:   "main",
+                regexp: /^\/$/ },
+            {   name:   "createPost",
+                regexp: /^\/post\/create$/ },
+            {   name:   "viewPost",
+                regexp: /^\/post\/(.*)$/ },
+            {   name:   "404",
+                regexp: /./ }
+    ] );
+
+    // Start application from currently clicked / used URL
+    (function( window ) {
+        History = window.History;
+
+        // Bind to StateChange Event
+        History.Adapter.bind( window, 'statechange', function () {
+            var State = History.getState();
+            router.route( parseURL( location.href ) );
+        });
+
+        jQuery( window ).trigger( "statechange" );
+        jQuery( window ).trigger( "contentLoaded", "body" );
+
+    }( window ) );
+} );
